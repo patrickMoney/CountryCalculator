83f559996fd963a4384c63ddcd8240635b7ec42e
Patrick Nguyen <patrick.nguyen@moneydashboard.com>
2015-05-11 15:37:21 +0100

    added tests

--- UnitTestProject1/CountryCalculatorTests.cs
+++ UnitTestProject1/CountryCalculatorTests.cs
@@ -90,5 +90,72 @@ namespace UnitTestProject1
 
             Assert.AreEqual(4, countries);
         }
+
+        [TestMethod]
+        public void Example7()
+        {
+            int[,] map = new int[,]{
+                {1,2,1,2},
+                {1,1,2,1}
+            };
+
+            int countries = CountryCalculator.CalculateCountries(map);
+
+            Assert.AreEqual(6, countries);
+        }
+
+        [TestMethod]
+        public void Example8()
+        {
+            int [,] map = new int [,]{
+                {}
+            };
+
+            int countries = CountryCalculator.CalculateCountries(map);
+
+            Assert.AreEqual(0, countries);
+        }
+
+
+        [TestMethod]
+        public void Example9()
+        {
+            int[,] map = new int[,] { 
+                {1,1,1,1,2},
+                {1,1,1,1,1}
+            };
+
+            int countries = CountryCalculator.CalculateCountries(map);
+
+            Assert.AreEqual(2, countries);
+        }
+
+        [TestMethod]
+        public void Example10()
+        {
+            int[,] map = new int[,]{
+                {1,2,3,3,1},
+                {1,1,2,0,1},
+                {2,2,3,3,0}
+            };
+
+            int countries = CountryCalculator.CalculateCountries(map);
+
+            Assert.AreEqual(9, countries);
+        }
+
+        [TestMethod]
+        public void Example11()
+        {
+            int[,] map = new int[,]{
+                {1,1,1,1,1},
+                {1,1,2,1,1},
+                {1,1,1,1,1}
+            };
+
+            int countries = CountryCalculator.CalculateCountries(map);
+
+            Assert.AreEqual(2, countries);
+        }
     }
 }
