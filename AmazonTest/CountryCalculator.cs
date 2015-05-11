8e7303b038915303e8444fbc36bfbec35702faa4
TomMDB <tom.verheyen@moneydashboard.com>
2015-05-11 14:28:45 +0100

    Code for country calculator

---
+++ AmazonTest/CountryCalculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonTest
+{
+    public class CountryCalculator
+    {
+        public static int CalculateCountries(int[,] map)
+        {
+            int countries = 0;
+            int totalRows = map.GetUpperBound(0);
+            int totalColumns = map.GetUpperBound(1);
+
+            for (int row = 0; row <= totalRows; row++)
+            {
+                for (int column = 0; column <= totalColumns; column++)
+                {
+                    int currentValue = map[row, column];
+
+                    // Check above
+                    if (row > 0)
+                    {
+                        int valueAbove = map[row - 1, column];
+
+                        if (valueAbove == currentValue)
+                            continue;
+                    }
+
+                    // Check left
+                    if (column > 0)
+                    {
+                        int valueLeft = map[row, column - 1];
+
+                        if (valueLeft == currentValue)
+                            continue;
+                    }
+
+                    // check if part of a country mapping up
+                    if (row > 0)
+                    {
+                        int countryLinkColumn = partOfCountryMappingUp(currentValue, row, column, totalColumns, map);
+
+                        if (countryLinkColumn > -1)
+                        {
+                            column = countryLinkColumn;
+                            continue;
+                        }
+                    }
+
+                    countries++;
+                }
+            }
+
+            return countries;
+        }
+
+        private static int partOfCountryMappingUp(int currentValue, int currentRow, int currentColumn, int totalColumns, int[,] map)
+        {
+            for (int column = currentColumn+ 1; column <= totalColumns; column++)
+            {
+                int valueOfNewPosition = map[currentRow, column];
+                int valueAbove = map[currentRow - 1, column];
+
+                if (valueOfNewPosition != currentValue)
+                    return -1;
+
+                if (valueAbove == currentValue)
+                    return column;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ UnitTestProject1/CountryCalculatorTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AmazonTest;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CountryCalculatorTests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            int[,] map = new int[,] { 
+                {1, 1, 1},
+                {2, 1, 2},
+                {2, 1, 2}
+            };
+
+            int countries = CountryCalculator.CalculateCountries(map);
+
+            Assert.AreEqual(3, countries);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            int[,] map = new int[,] { 
+                {1, 1, 2},
+                {2, 2, 2},
+                {2, 1, 1}
+            };
+
+            int countries = CountryCalculator.CalculateCountries(map);
+
+            Assert.AreEqual(3, countries);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            int[,] map = new int[,] { 
+                {1, 1, 2},
+                {2, 1, 2},
+                {1, 2, 1}
+            };
+
+            int countries = CountryCalculator.CalculateCountries(map);
+
+            Assert.AreEqual(6, countries);
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            int[,] map = new int[,] { 
+                {2, 1, 1},
+                {2, 2, 2},
+                {2, 1, 1}
+            };
+
+            int countries = CountryCalculator.CalculateCountries(map);
+
+            Assert.AreEqual(3, countries);
+        }
+
+        [TestMethod]
+        public void Example5()
+        {
+            int[,] map = new int[,] { 
+                {1, 1, 1, 1, 1, 1, 2},
+                {1, 1, 1, 1, 2, 2, 2},
+                {1, 1, 1, 2, 2, 1, 1}
+            };
+
+            int countries = CountryCalculator.CalculateCountries(map);
+
+            Assert.AreEqual(3, countries);
+        }
+
+        [TestMethod]
+        public void Example6()
+        {
+            int[,] map = new int[,] { 
+                {1, 1, 2},
+                {2, 2, 1},
+                {2, 1, 1}
+            };
+
+            int countries = CountryCalculator.CalculateCountries(map);
+
+            Assert.AreEqual(4, countries);
+        }
+    }
+}
